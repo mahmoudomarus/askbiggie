f81bf97001f7d83dd4065c042fd6ec7c56eaf1c6
mahmoudomarus <mahmoudomarus@gmail.com>
2025-07-11 19:47:15 -0700

    Fix Docker image architecture: use amd64 tag for Daytona compatibility

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -221,7 +221,7 @@ class Configuration:
     STRIPE_PRODUCT_ID_STAGING: str = 'prod_SCgIj3G7yPOAWY'
     
     # Sandbox configuration
-    SANDBOX_IMAGE_NAME = "mahmoudomarus/askbiggie-sandbox:latest"
+    SANDBOX_IMAGE_NAME = "mahmoudomarus/askbiggie-sandbox:amd64"
     SANDBOX_ENTRYPOINT = "/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf"
 
     # LangFuse configuration
